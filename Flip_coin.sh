0d76f0e9f2e69d181568b60b24a3e0e44a4886c1
abhishekpachkawade <abhishekpachkawade@gmail.com>
2021-07-27 15:42:20 +0530

    UPdat3

--- Flip_coin.sh
+++ Flip_coin.sh
@@ -1,20 +1,33 @@
 #!/bin/bash -x
 declare -A perheads
 declare -A pertails
-heads=0
-tails=0
+headsheads=0
+tailstails=0
+headstails=0
+tailsheads=0
 for ((i=0;i<21;i++))
 do
 result=$[RANDOM%2]
-if [ $result -eq 0 ]
+result1=$[RANDOM%2]
+if [ $result -eq 0 ] && [ $result1 -eq 1 ]
 then
-tails=$[$tails+1]
-else
-heads=$[$heads+1]
+tailsheads=$[$tailsheads+1]
+elif [ $result -eq 0 ] && [ $result -eq 0 ]
+then
+tailstails=$[$tailstails+1]
+elif [ $result -eq 1 ] && [ $result1 -eq 0 ]
+then
+headstails=$[$headstails+1]
+elif [ $result -eq 1 ] && [ $result1 -eq 1 ]
+then
+headsheads=$[$headsheads+1]
 fi
 done
-perhead[heads]=$[$heads*100/21]
-echo "Percentage of Heads: ${perhead[@]}"
-pertail[tails]=$[$tails*100/21]
-echo "Percentage of Tails: ${pertail[@]}"
-
+perheadhead=$[$headsheads*100/21]
+echo "Percentage of Heads Heads: $perheadhead"
+pertailtail=$[$tailstails*100/21]
+echo "Percentage of Tails Tails: $pertailtail"
+perheadtail=$[$headstails*100/21]
+echo "Percentage of Heads Tails: $perheadtail"
+pertailhead=$[$tailsheads*100/21]
+echo "Percentage of Tails Heads: $pertailhead"
