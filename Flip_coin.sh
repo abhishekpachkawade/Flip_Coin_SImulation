9e80e11260d995784b7d392b58eba0049f1ddc83
abhishekpachkawade <abhishekpachkawade@gmail.com>
2021-07-27 15:36:51 +0530

    update2

--- Flip_coin.sh
+++ Flip_coin.sh
@@ -1,3 +1,19 @@
 #!/bin/bash -x
-
-#welcome
+declare -A perheads
+declare -A pertails
+heads=0
+tails=0
+for ((i=0;i<21;i++))
+do
+result=$[RANDOM%2]
+if [ $result -eq 0 ]
+then
+tails=$[$tails+1]
+else
+heads=$[$heads+1]
+fi
+done
+perhead[heads]=$[$heads*100/21]
+echo "Percentage of Heads: ${perhead[@]}"
+pertail[tails]=$[$tails*100/21]
+echo "Percentage of Tails: ${pertail[@]}"
